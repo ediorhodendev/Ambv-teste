3ad845654c1fba8cb487bdad393254a5917688e4
Edio Rhoden <ediorhodendev@gmail.com>
2025-04-29 11:49:28 -0300

    alterações

--- template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/Dtos/CustomerDto.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/Dtos/CustomerDto.cs
@@ -7,6 +7,8 @@ namespace Ambev.DeveloperEvaluation.Application.Customers.Dtos
         public Guid Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-       
+
+        public string Phone { get; set; } = string.Empty;
+
     }
 }
